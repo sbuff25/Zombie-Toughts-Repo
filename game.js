9a7e85dddf38304c8ca898f81c51ca80595fae77
Spencer Buffum <spencerbuffum17@gmail.com>
2020-07-29 09:59:48 -0600

    Update game.js

--- game.js
+++ game.js
@@ -18,18 +18,28 @@ function showTextNode(textNodeIndex) {
         optionButtonsElement.removeChild(optionButtonsElement.firstChild)
     }
 
+    // textNode.options.forEach(option => {
+    //         if (showOption(option)) {
+    //             const button = document.createElement('button')
+    //             button.innerText = option.text
+    //             button.classList.add('btn')
+    //             button.addEventListener('click', () => selectOption(option))
+    //             optionButtonsElement.appendChild(button)
+    //         }
+    //     })
     textNode.options.forEach(option => {
-            if (showOption(option)) {
-                const button = document.createElement('button')
-                button.innerText = option.text
-                button.classList.add('btn')
-                button.addEventListener('click', () => selectOption(option))
-                optionButtonsElement.appendChild(button)
-            }
-        })
+        if (showOption(option)) {
+            const button = document.createElement('button')
+            button.innerText = option.text
+            button.classList.add('btn')
+            button.addEventListener('touch', () => selectOption(option))
+            optionButtonsElement.appendChild(button)
+        }
+    })
         //This is storing the choices being made in the console. these some of these choices will effect final outcome of game. 
     console.log(state);
 }
+
 
 function showOption(option) {
     return option.requiredState == null || option.requiredState(state)
