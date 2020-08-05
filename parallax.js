e79c6c7722cb6219380da11f1b1c7cb3c3610de9
Spencer Buffum <spencerbuffum17@gmail.com>
2020-08-05 08:49:03 -0600

    update

--- parallax.js
+++ parallax.js
@@ -1,25 +1,25 @@
-let bg = document.getElementById("bg");
-let boy = document.getElementById("boy");
-let pig = document.getElementById("pig");
-let brain = document.getElementById("brain");
-let ledge = document.getElementById("ledge");
-let ztText = document.getElementById("title_animation");
-let authorText = document.getElementById("author_text");
-let mtRepLogo = document.getElementById("mt_rep_logo");
+// let bg = document.getElementById("bg");
+// let boy = document.getElementById("boy");
+// let pig = document.getElementById("pig");
+// let brain = document.getElementById("brain");
+// let ledge = document.getElementById("ledge");
+// let ztText = document.getElementById("title_animation");
+// let authorText = document.getElementById("author_text");
+// let mtRepLogo = document.getElementById("mt_rep_logo");
 
-window.addEventListener('scroll', function(params) {
-    var value = window.scrollY;
+// window.addEventListener('scroll', function(params) {
+//     var value = window.scrollY;
 
-    bg.style.top = value * 0.5 + "px";
-    boy.style.left = value * 0.5 + "px";
-    pig.style.left = value * 0.4 + "px";
-    brain.style.left = value * 0.1 + "px";
-    ledge.style.top = value * 0.9 + "px";
-    ztText.style.left = value * 0.25 + "px";
-    authorText.style.top = value * 0.15 + "px";
+//     bg.style.top = value * 0.5 + "px";
+//     boy.style.left = value * 0.5 + "px";
+//     pig.style.left = value * 0.4 + "px";
+//     brain.style.left = value * 0.1 + "px";
+//     ledge.style.top = value * 0.9 + "px";
+//     ztText.style.left = value * 0.25 + "px";
+//     authorText.style.top = value * 0.15 + "px";
 
     
 
 
 
-});
+// });
