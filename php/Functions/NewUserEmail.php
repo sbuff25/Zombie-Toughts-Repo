0063b3ad82c2f20f04497a77d5b0de2c9ef85926
Kathryn Reese <reese.kathryn@outlook.com>
2020-08-09 21:10:09 -0600

    Attempt to fix email again

--- php/Functions/NewUserEmail.php
+++ php/Functions/NewUserEmail.php
@@ -3,10 +3,10 @@
     function new_user_email_body($email, $first_name, $last_name, $key){
         $body = '';
         $body .= "Dear " . $first_name . " " . $last_name . ",\n";
-        $body .= "Welcome to the Montana Repertory Theatre\'s Zombie Thoughts Website!\n";
-        $body .= "In order to confirm your account creation, please click on the following link to set your password:\n";
-        $body .= '<a href="https://www.montanarepzombiethoughts.com/php/NewUserPassword.php?key=' . $key . '&email='. $email .'&action=setpassword" target="_blank">https://www.montanarepzombiethoughts.com/php/NewUserPassword.php?key=' . $key . '&email='. $email .'&action=setpassword</a>\n';
-        $body .= "If you do not set your password within 4 days of your account creation, you must ask your administrator to recreate an account for you.\n";
+        $body .= "Welcome to the Montana Repertory Theatre's Zombie Thoughts Website!\n";
+        $body .= "In order to confirm your account creation, please click on the following link to set your password:\n\n";
+        $body .= 'https://www.montanarepzombiethoughts.com/php/NewUserPassword.php?key=' . $key . '&email='. $email ."&action=setpassword\n";
+        $body .= "If you do not set your password within 4 days of your account creation, you must ask your administrator to recreate an account for you.\n\n";
         $body .= "Thank you,\n";
         $body .= "Montana Repertory Theatre\n";
 
