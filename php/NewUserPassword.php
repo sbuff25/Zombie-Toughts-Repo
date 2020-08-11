dbb690c15cd6385cb8a1e7a4d11cb6917e490be9
Kathryn Reese <reese.kathryn@outlook.com>
2020-08-11 16:17:26 -0600

    Fix button name

--- php/NewUserPassword.php
+++ php/NewUserPassword.php
@@ -37,7 +37,7 @@
                     <input type="password" name="confirm_password" maxlength="15" required/>
                     <br>
                     <input type="hidden" name="email" value="<?php echo $email;?>"/>
-                    <input type="submit" value="set_new_user" />
+                    <input type="submit" value="Create Account" />
                 </form>
             <?php
             }
