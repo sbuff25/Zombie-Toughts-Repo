3ed3f812b714c15f03b1c4f65002e15b4821b309
Kathryn Reese <reese.kathryn@outlook.com>
2020-08-10 18:33:02 -0600

    Trying to fix email link

--- php/FormCheck.php
+++ php/FormCheck.php
@@ -1,8 +1,8 @@
 <?php 
 
-$ini_file = parse_ini_file('/home3/yqepxzmy/db.ini');
-$database = mysqli_connect ("localhost", $ini_file['username'], $ini_file['password'], $ini_file['db'])
-or die ('I cannot connect to the database.');
+require_once("./Classes/Database.php");
+$DatabaseObject = new Database();
+$database = $DatabaseObject->get_database();
     
 
     // function check_password($username_or_email, $password){
