074e7f1af1b5e2215ce1de42b9d8e0f54bb5a310
Kathryn Reese <reese.kathryn@outlook.com>
2020-08-12 19:02:46 -0600

    modal

--- php/AdminPage.php
+++ php/AdminPage.php
@@ -81,7 +81,7 @@
 
         <?php //} ?>
 
-        <button type="button" class='btn btn-primary' data-toggle="modal" data-target="./LinkForm.php">Generate Access Link</a>
+        <button type="button" class='btn btn-primary' data-toggle="modal" data-target="./LinkForm.php">Generate Access Link</button>
 
         
 
