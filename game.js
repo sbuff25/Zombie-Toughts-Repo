d504b287d91b6e533b7bef50d697149bd553e1e9
Spencer Buffum <spencerbuffum17@gmail.com>
2020-08-16 20:50:24 -0600

    Update game.js

--- game.js
+++ game.js
@@ -948,6 +948,14 @@ function onYouTubeIframeAPIReady() {
         height: '390',
         width: '640',
         videoId: Video,
+        playerVars:{
+            'autoplay': 0,
+            'controls': 0,
+            'modestbranding': 1,
+            'rel': 0,
+            'showInfo': 0,
+
+        },
         events: {
             'onReady': onPlayerReady,
             'onStateChange': onPlayerStateChange
