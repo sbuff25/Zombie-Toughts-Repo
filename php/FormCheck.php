99180402a502f027fc89015b75f03842e69e0f64
Kathryn Reese <reese.kathryn@outlook.com>
2020-08-17 18:43:01 -0600

    Sql injection prevention

--- php/FormCheck.php
+++ php/FormCheck.php
@@ -17,10 +17,16 @@ require_once("./Classes/Database.php");
 
 
         if (count($errors) == 0) {
-            $check_user = "SELECT email, username, first_name, last_name, salt FROM AdminUser WHERE email = '$email_or_username' OR username = '$email_or_username'";
-            $result = mysqli_query($database, $check_user);
-            if(mysqli_num_rows($result) == 1){
-                $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            // $check_user = "SELECT email, username, first_name, last_name, salt FROM AdminUser WHERE email = '$email_or_username' OR username = '$email_or_username'";
+            $stmt = $database.prepare("SELECT email, username, first_name, last_name, salt FROM AdminUser WHERE email = ? OR username = ?");
+            $stmt.bind_param("ss", $email_or_username, $email_or_username);
+            $stmt.execute();
+            //$result = mysqli_query($database, $check_user);
+            $result = $stmt.get_result();
+            //if(mysqli_num_rows($result) == 1){
+            if($result.num_rows() === 1){
+                //$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                $row = $result.fetch_assoc();
                 $hashed_password = hash_pbkdf2('haval256,5', $password, $row['salt'], 10, 70);
                 $check_pass1 = "SELECT EXISTS(SELECT * FROM AdminUser WHERE (password = '$hashed_password' AND username = '$email_or_username'))";
 
@@ -44,6 +50,7 @@ require_once("./Classes/Database.php");
             else{
                 array_push($errors, "Username, email, and/or password are incorrect.");
             }
+            $stmt.close();
         }
 
         
