61d4caa7b840776049110d914359a2db17976f3a
Kathryn Reese <reese.kathryn@outlook.com>
2020-08-29 16:50:37 -0600

    added phone format

--- InstitutionAccessForm.php
+++ InstitutionAccessForm.php
@@ -99,7 +99,7 @@ $("#ind_or_inst_select").on('change', function() {
             $(" <input name='contact_email' type='email' placeholder='Enter Email...' size='20'><br>").appendTo( "#add_here" );
 
             $(" <label for='contact_phone'>Contact's Phone Number:&nbsp;</label>").appendTo( "#add_here" );
-            $(" <input name='contact_phone' type='tel' placeholder='Enter Phone Number...' size='20' pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"><br><br>").appendTo( "#add_here" );
+            $(" <input name='contact_phone' type='tel' placeholder='Enter Phone Number...' size='20' pattern='([0-9]{3}) [0-9]{2}-[0-9]{3}'><br><br>").appendTo( "#add_here" );
 
 
             $(" <h5><b>Institution Information:</b></h5><br>").appendTo( "#add_here" );
@@ -135,7 +135,7 @@ $("#ind_or_inst_select").on('change', function() {
             $("<input name='email' type='email' placeholder='Enter Email...' size='20'><br>").appendTo( "#add_here" );
 
             $("<label for='phone'>Phone Number:&nbsp;</label>").appendTo( "#add_here" );
-            $("<input name='phone' type='tel' placeholder='Enter Phone Number...' size='20' pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"><br>").appendTo( "#add_here" );
+            $("<input name='phone' type='tel' placeholder='Enter Phone Number...' size='20' pattern='([0-9]{3}) [0-9]{2}-[0-9]{3}'><br>").appendTo( "#add_here" );
 
             $("<label for='ext'>Extension Number:&nbsp;</label>").appendTo( "#add_here" );
             $("<input name='ext' placeholder='Enter Extension...' size='20'><br>").appendTo( "#add_here" );
