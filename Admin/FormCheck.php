5c1994d11d6166b892d272f33b04708cb32fe84c
Kathryn Reese <reese.kathryn@outlook.com>
2020-09-14 21:20:11 -0600

    Fix form

--- Admin/FormCheck.php
+++ Admin/FormCheck.php
@@ -448,7 +448,7 @@ require_once("./Classes/Database.php");
                 total_number_of_accesses) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
         );
 
-        $stmt->bind_param("sssssissssssssssssii", 
+        if(!$stmt->bind_param("sssssissssssssssssii", 
             $code, 
             $first_name, 
             $last_name,
@@ -469,12 +469,15 @@ require_once("./Classes/Database.php");
             $couPhone,
             $couExt,
             $num_access
-        );
+        )){
+            array_push($errors, "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error);
+     
+        }
 
-        $stmt->execute();
+        // $stmt->execute();
 
-        if(!$stmt){
-            array_push($errors, "(Error Code: 0) There was a problem processing your request. Please try your request again, and if it still does not work please contact the Montana Repertory Theatre directly for access.");
+        if(!$stmt->execute()){
+            array_push($errors, "Execute failed: (" . $stmt->errno . ") " . $stmt->error);
         }
         $stmt->close();
       
@@ -483,27 +486,23 @@ require_once("./Classes/Database.php");
             $gradeQuery = "";
             $paramBind = "";
             foreach ($grades as $grade){
-                // $gradeQuery .= "(" . $grade . ", " . $code . ")";
+                $gradeQuery .= "(" . $grade . ", " . $code . ")";
                 //$paramBind .= "ss";
                 // $gradeQuery .= "( ? , ? )";
-                $stmt = $database->prepare("INSERT INTO InstGrades (grade, instCode) VALUES (?, ?)");
-                $stmt->bind_param("ss", $grade, $code);
-                $stmt->execute();
-
-                if(!$stmt){
-                    array_push($errors, "(Error Code: 1) There was a problem processing your request. Please try your request again, and if it still does not work please contact the Montana Repertory Theatre directly for access.");
-                }
-                $stmt->close();
             }
 
-            // $stmt = $database->prepare("INSERT INTO InstGrades (grade, instCode) VALUES ?");
-            // $stmt->bind_param("s", $gradeQuery);
-            // $stmt->execute();
+            $stmt = $database->prepare("INSERT INTO InstGrades (grade, instCode) VALUES ?");
+            if(!$stmt->bind_param("s", $gradeQuery)){
+                array_push($errors, "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error);
+            }
+            if(!$stmt->execute()){
+                array_push($errors, "Execute failed: (" . $stmt->errno . ") " . $stmt->error);
+            }
 
             // if(!$stmt){
             //     array_push($errors, "(Error Code: 1) There was a problem processing your request. Please try your request again, and if it still does not work please contact the Montana Repertory Theatre directly for access.");
             // }
-            // $stmt->close();
+            $stmt->close();
         }
 
         
