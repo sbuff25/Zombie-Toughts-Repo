dd04963e918f6221d61187f95e27d6cd6029824a
Kathryn Reese <reese.kathryn@outlook.com>
2020-10-07 22:41:35 -0600

    Try to fix institution code submit

--- accessCheck.php
+++ accessCheck.php
@@ -80,17 +80,19 @@ require_once("./Admin/Classes/Database.php");
                     array_push($errors, "The code you have entered has passed its access limit. If you are a student, please contact your teacher.");
                 }
                 $times_accessed = $times_accessed + 1;
-
-                // $stmt2 = $database->prepare("ALTER TABLE InstitutionAccessCode UPDATE COLUMN times_accessed=? WHERE code=?");
-                // if(!$stmt2->bind_param("is", $times_accessed, $code)){
-                //     array_push($errors, "Binding parameters failed: (" . $stmt2->errno . ") " . $stmt2->error);
+                $stmt2 = "";
+                if (!$stmt2 = $database->prepare("ALTER TABLE InstitutionAccessCode UPDATE COLUMN times_accessed=? WHERE code=?")) {
+                    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+                }
+                if(!$stmt2->bind_param("is", $times_accessed, $code)){
+                    array_push($errors, "Binding parameters failed: (" . $stmt2->errno . ") " . $stmt2->error);
                     
             
-                // }
+                }
     
-                // if(!$stmt2->execute()){
-                //     array_push($errors, "Execute failed: (" . $stmt2->errno . ") " . $stmt2->error);
-                // }
+                if(!$stmt2->execute()){
+                    array_push($errors, "Execute failed: (" . $stmt2->errno . ") " . $stmt2->error);
+                }
                 array_push($errors, $code . " " . $times_accessed);
                 // $stmt2->close();
 
