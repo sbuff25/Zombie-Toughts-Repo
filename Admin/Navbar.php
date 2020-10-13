f447a2b70a9fe9279218bfc407c31538323475da
Kathryn Reese <reese.kathryn@outlook.com>
2020-10-12 19:11:40 -0600

    Footer

--- Admin/Navbar.php
+++ Admin/Navbar.php
@@ -15,10 +15,10 @@
             <li class="nav-item active">
                 <a class="nav-link text-white" href="LoginPage?logout='1'">Logout</a>
             </li>
-            <li class="nav-item text-light">
+            <li class="nav-item text-secondary">
                 <p class='nav-link'>You are logged in as: <?php echo $_SESSION['email']; ?></p>
             </li>
-            <li class="nav-item text-light">
+            <li class="nav-item text-secondary">
                 <p class='nav-link'>Privilege: <?php echo $_SESSION['privilege']; ?></p>
             </li>
         <?php } 
