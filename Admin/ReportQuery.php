913188dfe5a70c3e758ec521c09889185eaac15e
Kathryn Reese <reese.kathryn@outlook.com>
2020-10-12 19:31:51 -0600

    Footer

--- Admin/ReportQuery.php
+++ Admin/ReportQuery.php
@@ -35,6 +35,7 @@ require_once("./Classes/Database.php");
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
         <link rel="stylesheet" href="./reports.css">
+        <link rel="stylesheet" href="./phpforms.css">
         
     
 
@@ -304,7 +305,7 @@ require_once("./Classes/Database.php");
         
 
         <!--Footer-->
-        <footer class="center bg-dark">
+        <footer class="sticky-footer center bg-dark">
             <p>Missoula Repertory Theatre &copy; 2020</p>
         </footer>
 
