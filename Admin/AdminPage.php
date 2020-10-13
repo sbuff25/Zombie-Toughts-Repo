bb1d2ad5f9f5aed4f89a2821166b3afd04cafe53
Kathryn Reese <reese.kathryn@outlook.com>
2020-10-12 18:29:43 -0600

    Footer

--- Admin/AdminPage.php
+++ Admin/AdminPage.php
@@ -22,15 +22,15 @@ include_once('FormCheck.php');
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-        <link rel="stylesheet" href="../css.css">
-        <link rel="stylesheet" href="../menu.css">
+        <!-- <link rel="stylesheet" href="../css.css">
+        <link rel="stylesheet" href="../menu.css"> -->
         
-        <link rel="stylesheet" href="../scroll_bar.css">
-        <link rel="stylesheet" href="../text_animiations.css">
+        <!-- <link rel="stylesheet" href="../scroll_bar.css">
+        <link rel="stylesheet" href="../text_animiations.css"> -->
 
 
     </head>
-    <body>
+    <body class='bg-dark'>
         <div class="col-md-8 mx-auto bg-light text-dark text-center" >
             <div class="panel panel-default justify-content-center">
             <?php
