bfd57ca9fb91efbd280a0b509359563d1729258d
Kathryn Reese <reese.kathryn@outlook.com>
2020-10-12 18:36:21 -0600

    Footer

--- Admin/Navbar.php
+++ Admin/Navbar.php
@@ -1,18 +1,25 @@
 <div id="progressbar"></div>
 <div id="scrollPath"></div>
 
-<nav class="navbar">
-    <ul class="nav_links">
+<nav class="navbar navbar-inverse">
+    <ul class="nav navbar-nav">
 
         <?php if(isset($_SESSION['email'])){
         ?>
             <li>
                 <a href="./AdminPage">Admin Home</a>
+            </li>
+            <li>
                 <a href="./RequestsPage">Requests</a>
+            </li>
+            <li>
                 <a href="LoginPage?logout='1'">Logout</a>
+            </li>
+            <li>
                 <p>You are logged in as: <?php echo $_SESSION['email']; ?></p>
+            </li>
+            <li>
                 <p>Privilege: <?php echo $_SESSION['privilege']; ?></p>
-
             </li>
         <?php } 
         else {?>
