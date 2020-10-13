2a4e3d110718fd3b25ffc069335541d913a34cc0
Kathryn Reese <reese.kathryn@outlook.com>
2020-10-12 18:38:28 -0600

    Footer

--- Admin/Navbar.php
+++ Admin/Navbar.php
@@ -28,9 +28,9 @@
             </li>
         <?php } ?>
     </ul>
-    <div class="burger">
+    <!-- <div class="burger">
     <div class="line1"></div>
     <div class="line2"></div>
-    <div class="line3"></div>
+    <div class="line3"></div> -->
     </div>
 </nav>
