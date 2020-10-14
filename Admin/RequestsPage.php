8e6d4f2fa1dea306b00d666efc2772e142a93b86
Kathryn Reese <reese.kathryn@outlook.com>
2020-10-13 21:08:54 -0600

    fix table

--- Admin/RequestsPage.php
+++ Admin/RequestsPage.php
@@ -131,18 +131,18 @@ include_once('FormCheck.php');
                                     <th colspan='12' class='text-center'><b>Montana Residents Zombie Thought Requests</b></th>
                                 </tr>
                                 <tr>
-                                    <th scope='col'>Date Requested</th>
-                                    <th scope='col'>Contact First Name</th>
-                                    <th scope='col'>Contact Last Name</th>
-                                    <th scope='col'>Contact Phone Number</th>
-                                    <th scope='col'>Contact Extension (if available)</th>
-                                    <th scope='col'>Contact Email</th>
-                                    <th scope='col'>Institution Name</th>
-                                    <th scope='col'>Institution City</th>
-                                    <th scope='col'>Institution County</th>
-                                    <th scope='col'>Contacted</th>
-                                    <th scope='col'>Notes</th>
-                                    <th scope='col'>Options</th>
+                                    <th scope='col' onclick="sortTable(0)">Date Requested</th>
+                                    <th scope='col' onclick="sortTable(1)">Contact First Name</th>
+                                    <th scope='col' onclick="sortTable(2)">Contact Last Name</th>
+                                    <th scope='col' onclick="sortTable(3)">Contact Phone Number</th>
+                                    <th scope='col' onclick="sortTable(4)">Contact Extension (if available)</th>
+                                    <th scope='col' onclick="sortTable(5)">Contact Email</th>
+                                    <th scope='col' onclick="sortTable(6)">Institution Name</th>
+                                    <th scope='col' onclick="sortTable(7)">Institution City</th>
+                                    <th scope='col' onclick="sortTable(8)">Institution County</th>
+                                    <th scope='col' onclick="sortTable(9)">Contacted</th>
+                                    <th scope='col' onclick="sortTable(10)">Notes</th>
+                                    <th scope='col' onclick="sortTable(11)">Options</th>
 
                                 </tr>
                             </thead>
@@ -563,17 +563,61 @@ include_once('FormCheck.php');
         }
     </script>     -->
     <script>
-        $(document).ready(function () {
-        $('#mt-sort').DataTable({
-        "aaSorting": [],
-        columnDefs: [{
-        orderable: false,
-        targets: 3
-        }]
-        });
-        $('.dataTables_length').addClass('bs-select');
-        });
-    </script>
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("myTable2");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+</script>
 
     </body>
 </html>
