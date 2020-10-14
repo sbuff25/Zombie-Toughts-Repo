e3d8bb5ff55fa11db43c926bfa526d707c07371e
Kathryn Reese <reese.kathryn@outlook.com>
2020-10-13 19:07:18 -0600

    fix table

--- Admin/RequestsPage.php
+++ Admin/RequestsPage.php
@@ -72,7 +72,7 @@ include_once('FormCheck.php');
         </div>
         <?php endif ?>
 
-        <div class='tab-content'>
+        <div class='tab-content col-lg-10 mx-auto'>
 
             <!-- __________________________________Montana Requests Table______________________________ -->
             <div class='tab-pane container active' id='MontanaInstitution'>
