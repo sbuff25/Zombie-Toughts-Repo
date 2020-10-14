e67e2be6283c15a3f11205f2f8ffd90485ad082b
Kathryn Reese <reese.kathryn@outlook.com>
2020-10-13 20:26:02 -0600

    fix table

--- Admin/RequestsPage.php
+++ Admin/RequestsPage.php
@@ -94,7 +94,7 @@ include_once('FormCheck.php');
                         $order='ASC';
                     }
                     
-                    echo $_SESSION['sortby'];
+                    echo $sortby;
                 ?>
                 <form action='RequestsPage' method='POST'>
                     <select id='sortby' name='sortbyselect'>
